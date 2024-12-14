bb451153ad2efd43dcc2eca669d538eb4d7f714e
HenryCanoB <Henry.Canob@gmail.com>
2024-12-13 21:01:40 -0500

    en rama mod

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <>
       <div className="container">
         <div className='jumbotron'>
-          <p>Hola Cibertec Modificado</p>
+          <p>Hola Cibertec Modificado en rama mod</p>
         </div>
         <button className='btn btn-danger'>boton bootstrap</button>
         <div className="alert alert-primary" role="alert">
