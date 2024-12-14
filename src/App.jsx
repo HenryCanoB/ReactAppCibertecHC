7b260a3b971c1a3fef7c696f8f2c94325db461a7
HenryCanoB <Henry.Canob@gmail.com>
2024-12-13 20:06:14 -0500

    Commit Inicial

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className='jumbotron'>
+          <p>Hola Cibertec C</p>
+        </div>
+        <button className='btn btn-danger'>boton bootstrap</button>
+        <div className="alert alert-primary" role="alert">
+          A simple primary alert—check it out!
+        </div>
+        <div className="alert alert-secondary" role="alert">
+          A simple secondary alert—check it out!
+        </div>
+        <div className="alert alert-success" role="alert">
+          A simple success alert—check it out!
+        </div>
+        <div className="alert alert-danger" role="alert">
+          A simple danger alert—check it out!
+        </div>
+        <div className="alert alert-warning" role="alert">
+          A simple warning alert—check it out!
+        </div>
+        <div className="alert alert-info" role="alert">
+          A simple info alert—check it out!
+        </div>
+        <div className="alert alert-light" role="alert">
+          A simple light alert—check it out!
+        </div>
+        <div className="alert alert-dark" role="alert">
+          A simple dark alert—check it out!
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
